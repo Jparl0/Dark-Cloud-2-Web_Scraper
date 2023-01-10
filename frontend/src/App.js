59f6bea13e6fcf9d00ac4f57a2b5e9b401d86231
Joseph <joeyparlamenti@yahoo.com>
2023-01-09 21:34:09 -0500

    basic single-page layout CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import HomePage from "./Components/HomePage";
+import HomePage from "./Components/HomePage.js";
 
 import './App.css';
 
--- frontend/src/Components/HomePage.js
+++ frontend/src/Components/HomePage.js
@@ -4,23 +4,29 @@ function HomePage() {
 
     const [ searchInventValue, setSearchInventValue] = useState("");
 
-
-
     function handleInventVal(e) {
         setSearchInventValue(e.target.value)
     }
+    // console.log(searchInventValue)
 
   return (
     <div>
 
         <header>
-            <h1>
+            <h1 id='title'>
                 Dark Cloud 2 Crafting Data
             </h1>
+
+            {/* description */}
+            <div id='header-description'>
+                <p>
+                    This web application utilizes Go to query over a large body of text to extract key information. This will allow users to better conceptualize the data needed for specific classes
+                </p>
+            </div>
         </header>
 
         {/* Main Section Box */}
-        <div className='main-section'>
+        <section id='main-section'>
 
             {/* Ideas */}
                 {/* <div>
@@ -41,22 +47,17 @@ function HomePage() {
             {/* Inventions */}
                 <div>
                     <h2 className='title'>Inventions Section</h2>
+                    <p> Use the search bar below to find the requirements for any Invention*</p>
                     <div id='main-invent-container'>
                         <form>
-                            <label>Name Search:</label>
+                            <label>Name:</label>
                             <input type='text' id='invent-name-search' onChange={handleInventVal}></input>
-
                         </form>
-
                     </div>
                 </div>
-
-        </div>
-
+        </section>
         {/* <footer>
-
         </footer> */}
-
     </div>
   )
 }
