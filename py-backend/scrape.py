887af3d34d71f18097f40cbf352d418ea7557280
JoeyParlamenti <Joeyparlamenti@yahoo.com>
2023-02-19 22:13:46 -0500

    updated BeautifulSoup functions, corrected initial mapping issue

--- py-backend/scrape.py
+++ py-backend/scrape.py
@@ -1,4 +1,5 @@
 import requests
+from dc2types import Invention
 from bs4 import BeautifulSoup
 
 #  Get Request
@@ -14,44 +15,49 @@ invents_sect = invents.find("div", class_='section-original')
 
 invents_sect2 = invents_sect.find("div", class_='fr-view box light guide no-top-border')
 
-invents_table = invents_sect2.find_next('table')
-
-content = invents_sect.find_all('tr')
-
-
-
+content = invents_sect2.tbody
+invent_Dict = {
+    "invent1": {}
+}
 
 def inventionMapper():
 
-    for x in content:
-        i = 1
-        
-        if i == 1:
-            mapped_invent_name = x
-            return mapped_invent_name
-        elif i == 2:
-            mapped_idea1 = x
-            return mapped_idea1
-        elif i == 3:
-            mapped_idea2 = x
-            return mapped_idea2
-        elif i == 4:
-            mapped_idea3 = x
-            return mapped_idea3
-        else:
-            mapped_descript = x
-            i = 1
-            return mapped_descript
+    temp_invent_Dict = {
+        "name" = "",
+        "ideas" = ["", "", ""],
+        "description"
+    }
+    for tr in content:
+        temp_invention = ["", "", "", "", ""]
+        i = 0
+        for td in tr:
+            i += 1
+            if i == 1:
+                mapped_invent_name = td
+                temp_invention[0] = mapped_invent_name
+            elif i == 2:
+                mapped_idea1 = td
+                temp_invention[1] = mapped_idea1
+            elif i == 3:
+                mapped_idea2 = td
+                temp_invention[2] = mapped_idea2
+            elif i == 4:
+                mapped_idea3 = td
+                temp_invention[3] = mapped_idea3
+            else:
+                mapped_descript = td
+                temp_invention[4] = mapped_descript
+                
+                return temp_invention
+                # return temp_invention
+            
 
-        i += 1
-        
-
+                
     
-
-
-
 print(inventionMapper())
+
 # print(content)
+# print(invents_sect2)
 # print(soup.prettify())
 
 # https://psnprofiles.com/guide/3895-dark-cloud-2-ideas-scoops-and-inventions-guide
